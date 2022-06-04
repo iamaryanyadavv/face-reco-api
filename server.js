6397c14bb5701a9100e1034e199fbeb955d04b47
iamaryanyadavv <aryanyadav1601@gmail.com>
2022-06-05 02:17:08 +0530

    finished server

---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const app = express()
+app.use(bodyParser.json());
+app.use(cors());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'john',
+            password: 'cookies',
+            email: 'john@gmail.com',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'sally',
+            password: 'bananas',
+            email: 'sally@gmail.com',
+            entries: 0,
+            joined: new Date()
+        }
+
+    ]
+}
+
+app.get('/',(req,res)=>{
+    res.send(database.users);
+})
+
+app.post('/signin', (req,res)=> {
+    if(req.body.email === database.users[0].email && 
+        req.body.password === database.users[0].password){
+            res.json(database.users[0]);
+        }
+    else{
+        res.status(400).json('error');
+    }
+})
+
+app.post('/register', (req,res)=>{
+    const {email, name} = req.body;
+    database.users.push({
+        id: '125',
+            name: name,
+            email: email,
+            entries: 0,
+            joined: new Date()
+    })
+    res.json(database.users[database.users.length-1])
+})
+
+app.get('/profile/:id', (req,res)=>{
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id){
+            found = true;
+            return res.json(user[0]);
+        }
+    })
+    if(!found){
+        res.status(400).json('not found');
+    }
+})
+
+app.put('/image',(req,res)=>{
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id){
+            found = true;
+            user.entries += 1;
+            return res.json(user.entries);
+        }
+    })
+    if(!found){
+        res.status(400).json('not found');
+    }
+})
+
+
+
+app.listen(3000, ()=>{
+    console.log('running on port 3000')
+})
